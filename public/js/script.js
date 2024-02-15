4da29098ab9b0090993d922b33771afc13ec1f6b
David Moore <dtm819@gmail.com>
2024-02-15 18:44:18 -0500

    added deployed to readme

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,3 @@
-
 let myEvents = [];
 const pushEvents = async () => {
 const response = await fetch('/api/calendar/events', {
@@ -12,50 +11,64 @@ const response = await fetch('/api/calendar/events', {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  var calendarEl = document.getElementById("calendar");
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: "dayGridMonth",
-    selectable: true,
-    droppable: true,
-    editable: true,
-    events: myEvents,
-    eventClick: function (info) {
-      alert("Event: " + info.event.title);
-    },
-    headerToolbar: {
-      left: "prev,next today",
-      center: "title",
-      right: "dayGridMonth,timeGridWeek,timeGridDay",
-    }
-  });
-
-
-  document.getElementById("eventButton").addEventListener("click", function () {
-
-
-    let task = prompt("Would you like to add an event? (Yes or No):");
-    if (task.toLowerCase() === "yes") {
-      let eventName = prompt("Enter the name of your new event:");
-      let startDate = prompt("Enter the date of your event (Year-Month-Day ex: 2024-01-01):");
-      let eventTime = prompt("What time is your event?:");
-      let newArr = {title: eventName, start: startDate, time: eventTime};
-      myEvents.push(newArr);
-      calendar.addEvent(newArr);
-    }
-  });
+  pushEvents()
+  .then(() => {
+    console.log(myEvents);
+    var calendarEl = document.getElementById("calendar");
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: "dayGridMonth",
+      selectable: true,
+      droppable: true,
+      editable: true,
+      events: myEvents,
   
-  console.log(myEvents);
-  calendar.render();
-
-  var filterSelect = document.getElementById("filterSelect");
-
-  filterSelect.addEventListener("change", function () {
-    var selectedFilter = filterSelect.value;
-
-    var filteredEvents = events.filter(function (event) {
-      return selectedFilter === "all" || event.dataFilter === selectedFilter;
+      eventClick: function (info) {
+        alert("Event: " + info.event.title);
+      },
+      headerToolbar: {
+        left: "prev,next",
+        center: "title",
+        right: "dayGridMonth,timeGridWeek,timeGridDay",
+      }
     });
+  
+    document.getElementById("pushEvent").addEventListener("click", function () {
+      
+      let title = document.querySelector('#eventTitle').value.trim();
+      let start = document.querySelector('#eventDate').value.trim();
+  
+        let newArr = {title: title, start: start};
+        myEvents.push(newArr)
+        calendar.addEvent(newArr);
 
-    calendar.setOption("events", filteredEvents);
-  });
+        if (title && start) {
+          const response = fetch(`/api/calendar`, {
+            method: 'POST',
+            body: JSON.stringify({ title, start}),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+          if (response.ok) {
+            document.location.replace('/');
+          } 
+        }
+      
+      });
+    
+    calendar.render();
+  
+    var filterSelect = document.getElementById("filterSelect");
+  
+    filterSelect.addEventListener("change", function () {
+      var selectedFilter = filterSelect.value;
+  
+      var filteredEvents = events.filter(function (event) {
+        return selectedFilter === "all" || event.dataFilter === selectedFilter;
+      });
+  
+      calendar.setOption("events", filteredEvents);
+    });
+  })
+
 });
